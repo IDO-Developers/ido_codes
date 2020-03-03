a02e52635b1ce69987fbbd4d8b81890d75506b3a
Cristian Diaz <43429236+KrizPLAY@users.noreply.github.com>
2020-03-03 11:46:23 -0600

    update

---
+++ src/clases/usuarios.java
@@ -0,0 +1,33 @@
+package clases;
+
+public class usuarios {
+
+	String RNE_Empleado;
+	String password;
+	String Id_Rol;
+
+	public String getRNE_Empleado() {
+		return RNE_Empleado;
+	}
+
+	public void setRNE_Empleado(String rNE_Empleado) {
+		RNE_Empleado = rNE_Empleado;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getId_Rol() {
+		return Id_Rol;
+	}
+
+	public void setId_Rol(String id_Rol) {
+		Id_Rol = id_Rol;
+	}
+
+}
