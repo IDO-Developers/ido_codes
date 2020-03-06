8be749cd8b03979e093387231f76609ea8417801
Cristian Diaz <43429236+KrizPLAY@users.noreply.github.com>
2020-03-06 11:40:17 -0600

    UPDATE

---
+++ src/ventanas/iniciar.java
@@ -0,0 +1,12 @@
+package ventanas;
+
+public class iniciar {
+
+	public static void main(String[] args) {
+		login login = new login();
+		login.setLocationRelativeTo(null);
+		login.setVisible(true);
+
+	}
+
+}
