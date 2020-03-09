e45e37d232e7a43d8054398a1f434582c18abcea
Cristian Diaz <43429236+KrizPLAY@users.noreply.github.com>
2020-03-09 11:57:29 -0600

    update

---
+++ src/clases/alumnos.java
@@ -0,0 +1,33 @@
+package clases;
+
+public class alumnos {
+
+	String RNE_Alumno;
+	String password;
+	String Id_Grupo;
+
+	public String getRNE_Alumno() {
+		return RNE_Alumno;
+	}
+
+	public void setRNE_Alumno(String rNE_Alumno) {
+		RNE_Alumno = rNE_Alumno;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getId_Grupo() {
+		return Id_Grupo;
+	}
+
+	public void setId_Grupo(String id_Grupo) {
+		Id_Grupo = id_Grupo;
+	}
+
+}
